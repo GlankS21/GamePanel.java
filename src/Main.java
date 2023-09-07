7db8c97ec114ce15908478c84dfbae19a727f37e
asgat <asgat@DESKTOP-VE6NRS6>
2023-09-07 22:24:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Game Game = new Game();
+
+        Game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Game.setVisible(true);
+    }
+}
